138c9cab7e2491ea12b6ef79231eec49bd4eccbd
prascle <prascle>
2003-06-29 13:17:10 +0000

    PR: needed by merge C. CAREMOLI branch CCAR_br1

---
+++ src/SMESH_SWIG/SMESH_shared_modules.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+# force SMESH importation at interpretor initialization
+# see salome_shared_modules.py
+# (avoids incomplete import at run time)
+
+print "============== import SMESH ======================="
+
+import SMESH
+
+# this function is required
+
+def init_shared_modules():
+   """
+      This function initializes shared modules that need to be
+   """
+   pass
