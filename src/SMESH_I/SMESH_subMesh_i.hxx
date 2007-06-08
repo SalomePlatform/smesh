0b14381aacc0a0a964865aaff21cb63cafc8a090
eap <eap@opencascade.com>
2007-06-08 14:09:33 +0000

    PAL16099 (Hypothesis modified and Mesh icon is still "Completed") +  void changeLocalId(int localId) { _localId = localId; } +  friend void SMESH_Mesh_i::CheckGeomGroupModif();

--- src/SMESH_I/SMESH_subMesh_i.hxx
+++ src/SMESH_I/SMESH_subMesh_i.hxx
@@ -36,6 +36,7 @@
 #include CORBA_CLIENT_HEADER(MED)
 
 #include "SALOME_GenericObj_i.hh"
+#include "SMESH_Mesh_i.hxx"
 
 class SMESH_Gen_i;
 class SMESH_Mesh_i;
@@ -86,8 +87,11 @@ public:
   SMESH_Mesh_i* _mesh_i; //NRI
 
 protected:
+  void changeLocalId(int localId) { _localId = localId; }
   SMESH_Gen_i* _gen_i;
   int _localId;
+
+  friend void SMESH_Mesh_i::CheckGeomGroupModif();
 };
 
 #endif
