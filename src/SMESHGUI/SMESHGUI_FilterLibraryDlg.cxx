06bdce2fefc91c41f1f1eac3d1ea1a6598163cb7
asl <asl@opencascade.com>
2008-04-30 05:33:05 +0000

    PAL19669: sensitive zone of OK button is very small

--- src/SMESHGUI/SMESHGUI_FilterLibraryDlg.cxx
+++ src/SMESHGUI/SMESHGUI_FilterLibraryDlg.cxx
@@ -256,9 +256,6 @@ QWidget* SMESHGUI_FilterLibraryDlg::createButtonFrame (QWidget* theParent)
   myButtons[ BTN_OK    ] = new QPushButton(tr("SMESH_BUT_OK"   ), aGrp);
   myButtons[ BTN_Apply ] = new QPushButton(tr("SMESH_BUT_APPLY"), aGrp);
 
-  QLabel* aLbl = new QLabel(aGrp);
-  aLbl->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed));
-
   myButtons[ BTN_Cancel ] = new QPushButton(tr("SMESH_BUT_CANCEL"), aGrp);
   myButtons[ BTN_Close  ] = new QPushButton(tr("SMESH_BUT_CLOSE"),  aGrp);
   myButtons[ BTN_Help   ] = new QPushButton(tr("SMESH_BUT_HELP"),   aGrp);
