90d66ea704a63a2fd8d0a73f067dcbc7c1e8a8b0
abd <abd@opencascade.com>
2008-10-10 09:18:11 +0000

    Fix win32 compilation problem

--- src/StdMeshersGUI/StdMeshersGUI_DistrPreview.cxx
+++ src/StdMeshersGUI/StdMeshersGUI_DistrPreview.cxx
@@ -18,8 +18,6 @@
 // See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 //
 
-using namespace std;
-
 #include "StdMeshersGUI_DistrPreview.h"
 
 #include <Expr_NamedUnknown.hxx>
