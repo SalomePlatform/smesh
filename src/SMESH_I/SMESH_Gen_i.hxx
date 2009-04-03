908ed4f2a285deec72b4407f7ab3979c306f1ca8
eap <eap@opencascade.com>
2009-04-03 14:30:42 +0000

    0020210: EDF SMESH 976: Update of a smesh group after modification of the associated geom group +  static void SetPixMap(SALOMEDS::SObject_ptr theSObject, +                        const char*           thePixMap);

--- src/SMESH_I/SMESH_Gen_i.hxx
+++ src/SMESH_I/SMESH_Gen_i.hxx
@@ -464,6 +464,9 @@ public:
                       const char*           theName,
                       const char*           theDefaultName = 0);
 
+  static void SetPixMap(SALOMEDS::SObject_ptr theSObject,
+                        const char*           thePixMap);
+
   //  Get study context
   StudyContext* GetCurrentStudyContext();
 
