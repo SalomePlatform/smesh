f31477795020e2cfc50d5b625579285c644a6a96
dmv <dmv@opencascade.com>
2009-10-12 13:47:12 +0000

    correct previous integration (Porting to Python 2.6)

--- src/SMESH_SWIG/ex30_tepal.py
+++ src/SMESH_SWIG/ex30_tepal.py
@@ -1,4 +1,3 @@
-#! /usr/bin/python
 #  -*- coding: iso-8859-1 -*-
 # CEA/LGLS 2008, Christian Van Wambeke (CEA/LGLS), Francis KLOSS (OCC)
 # ====================================================================
