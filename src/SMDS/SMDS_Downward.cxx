4b5fd249abfca52265832f10435f18f5bb9c69ae
vsr <vsr@opencascade.com>
2011-06-02 05:57:35 +0000

    Update copyright

--- src/SMDS/SMDS_Downward.cxx
+++ src/SMDS/SMDS_Downward.cxx
@@ -1,9 +1,25 @@
-/*
- * SMDS_Downward.cxx
- *
- *  Created on: Jun 3, 2010
- *      Author: prascle
- */
+// Copyright (C) 2010-2011  CEA/DEN, EDF R&D, OPEN CASCADE
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
+
+// File: SMDS_Downward.cxx
+// Created: Jun 3, 2010
+// Author: prascle
 
 #include "SMDS_Downward.hxx"
 #include "SMDS_Mesh.hxx"
