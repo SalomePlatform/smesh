f190e6cfb1f16b5216984bb5a8fec85eb74d241f
jfa <jfa@opencascade.com>
2012-08-09 11:15:15 +0000

    Mantis issue 0021668: [CEA 564] MED2.1 to MED2.3

--- src/DriverMED/DriverMED_Family.cxx
+++ src/DriverMED/DriverMED_Family.cxx
@@ -357,9 +357,9 @@ DriverMED_Family::GetFamilyInfo(const MED::PWrapper& theWrapper,
   string aValue = aStr.str();
   // PAL19785,0019867 - med forbids whitespace to be the last char in the name
   int maxSize;
-  if ( theWrapper->GetVersion() == MED::eV2_1 )
-    maxSize = MED::GetNOMLength<MED::eV2_1>();
-  else
+  //if ( theWrapper->GetVersion() == MED::eV2_1 )
+  //  maxSize = MED::GetNOMLength<MED::eV2_1>();
+  //else
     maxSize = MED::GetNOMLength<MED::eV2_2>();
   int lastCharPos = min( maxSize, (int) aValue.size() ) - 1;
   while ( isspace( aValue[ lastCharPos ] ))
