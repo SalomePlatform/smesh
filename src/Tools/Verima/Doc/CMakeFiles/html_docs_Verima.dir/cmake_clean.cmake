b9cd996c2076b9cc0206f151ff9a8c4c5dd7d728
rnc <rnc@opencascade.com>
2013-10-14 15:02:25 +0000

    REG: Verima / Build procedure: Updated the CMake build procedure and removed some files previously added by mistake

--- src/Tools/Verima/Doc/CMakeFiles/html_docs_Verima.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/html_docs_Verima"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/html_docs_Verima.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
