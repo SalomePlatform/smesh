8fff0ccaded5504f785ff689fea994ea208f1490
eap <eap@opencascade.com>
2014-03-13 17:03:29 +0400

    022491: EDF 2249 SMESH: Integration of a small python library for quadrangle meshing
     Initial version

---
+++ src/Tools/MacMesh/MacMesh/Config.py
@@ -0,0 +1,29 @@
+dz = 1
+debug = 1
+
+try : ListObj
+except NameError : ListObj = []
+
+try: Connections
+except NameError: Connections = []
+
+try : publish
+except NameError : publish = 1
+
+try : Groups
+except NameError : Groups = []
+
+try : StudyName
+except NameError : StudyName = "Compound"
+
+try : Criterion
+except NameError : Criterion = 1
+
+try : Count
+except NameError : Count = 0
+
+try : RefPts
+except NameError : RefPts = []
+
+try : DirIndex
+except NameError : DirIndex = 0
