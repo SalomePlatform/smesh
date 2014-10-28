dba24849451f408a506e500550926eef5f35ce1b
vsr <vsr@opencascade.com>
2014-10-28 15:23:39 +0300

    Windows portability: additional fix to previous commit

--- src/SMESHGUI/SMESHGUI_HypothesesUtils.h
+++ src/SMESHGUI/SMESHGUI_HypothesesUtils.h
@@ -120,7 +120,7 @@ namespace SMESH
 
   SMESHGUI_EXPORT
   // name of proprty saving plug-in of a hypothesis
-  const char* Plugin_Name() { return "PLUGIN_NAME"; }
+  inline const char* Plugin_Name() { return "PLUGIN_NAME"; }
 }
 
 #endif // SMESHGUI_HYPOTHESESUTILS_H
