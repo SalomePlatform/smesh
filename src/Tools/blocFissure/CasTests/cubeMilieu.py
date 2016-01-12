9afabd719b1a9d990fd1a4a5405cb699d433669e
Paul RASCLE <paul.rascle@edf.fr>
2016-01-12 17:21:43 +0100

    correction cas test cubeMilieu (indice edge fond)

--- src/Tools/blocFissure/CasTests/cubeMilieu.py
+++ src/Tools/blocFissure/CasTests/cubeMilieu.py
@@ -6,7 +6,7 @@ from blocFissure import gmu
 dicoParams = dict(nomCas            = 'cubeMilieu',
                   maillageSain      = os.path.join(gmu.pathBloc, 'materielCasTests/cubeFin.med'),
                   brepFaceFissure   = os.path.join(gmu.pathBloc, "materielCasTests/cubeFin_Milieu.brep"),
-                  edgeFissIds       = [4],
+                  edgeFissIds       = [7],
                   lgInfluence       = 50,
                   meshBrep          = (5,10),
                   rayonPipe         = 10,
