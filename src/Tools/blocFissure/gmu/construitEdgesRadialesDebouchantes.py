596f40f5981b792f2edff687ed42a7ebbfe98d49
Paul RASCLE <paul.rascle@edf.fr>
2016-12-05 17:54:39 +0100

    debug blocFissure: CasTests.ellipse_1

--- src/Tools/blocFissure/gmu/construitEdgesRadialesDebouchantes.py
+++ src/Tools/blocFissure/gmu/construitEdgesRadialesDebouchantes.py
@@ -12,12 +12,12 @@ from sortEdges import sortEdges
 def construitEdgesRadialesDebouchantes(idisklim, idiskout, gptsdisks, raydisks,
                                        facesPipePeau, edgeRadFacePipePeau, nbsegCercle):
   """
-  construction des listes d'edges radiales sur chaque extrémité débouchante 
+  construction des listes d'edges radiales sur chaque extrémité débouchante
   """
   logging.info('start')
-    
+
   # --- listes de nappes radiales en filling à chaque extrémité débouchante
-  
+
   facesDebouchantes = [False, False]
   idFacesDebouchantes = [-1, -1] # contiendra les indices des faces disque débouchantes (facesPipePeau)
   listNappes =[]
@@ -47,7 +47,7 @@ def construitEdgesRadialesDebouchantes(idisklim, idiskout, gptsdisks, raydisks,
         geomPublish(initLog.debug, nappe, name)
         facesDebouchantes[i] = True
     listNappes.append(nappes)
-      
+
   # --- mise en correspondance avec les indices des faces disque débouchantes (facesPipePeau)
   for i, nappes in enumerate(listNappes):
     if facesDebouchantes[i]:
@@ -82,27 +82,48 @@ def construitEdgesRadialesDebouchantes(idisklim, idiskout, gptsdisks, raydisks,
           else:
             maxl = geompy.BasicProperties(edge)[0]
           if maxl < 0.01: # problème MakeSection
-            logging.debug("problème MakeSection recherche edge radiale %s, longueur trop faible: %s, utilisation partition", k, maxl)
+            logging.info("problème MakeSection recherche edge radiale %s, longueur trop faible: %s, utilisation partition", k, maxl)
             partNappeFace = geompy.MakePartition([face, nappes[k]], [] , [], [], geompy.ShapeType["FACE"], 0, [], 0)
             edps= geompy.ExtractShapes(partNappeFace, geompy.ShapeType["EDGE"], False)
             ednouv = []
             for ii, ed in enumerate(edps):
+              dmax=100.
               vxs = geompy.ExtractShapes(ed, geompy.ShapeType["VERTEX"], False)
               distx = [geompy.MinDistance(vx, face) for vx in vxs]
               distx += [geompy.MinDistance(vx, nappes[k]) for vx in vxs]
               dmax = max(distx)
-              logging.debug("  dmax %s",dmax)
-              if dmax < 0.01:
+              lgedge = geompy.BasicProperties(ed)[0]
+              logging.debug("  dmax %s, longueur edge %s",dmax, lgedge)
+              if dmax < 0.01 and  lgedge > 0.01:
                 ednouv.append(ed)
-            logging.debug("  edges issues de la partition: %s", ednouv)
-            for ii, ed in enumerate(ednouv):
-              geomPublish(initLog.debug, ed, "ednouv%d"%ii)
-            [edsorted, minl,maxl] = sortEdges(ednouv)
-            logging.debug("  longueur edge trouvée: %s", maxl) 
-            edge = edsorted[-1]
+            if (len(ednouv) > 0):
+              logging.debug("  edges issues de la partition: %s", ednouv)
+              for ii, ed in enumerate(ednouv):
+                geomPublish(initLog.debug, ed, "ednouv%d"%ii)
+              [edsorted, minl,maxl] = sortEdges(ednouv)
+              logging.debug("  longueur edge trouvée: %s", maxl)
+              edge = edsorted[-1]
+            else:
+              logging.info("problème partition recherche edge radiale %s", k)
+              vxs = geompy.ExtractShapes(partNappeFace, geompy.ShapeType["VERTEX"], False)
+              vxnouv=[]
+              for ii,vx in enumerate(vxs):
+                distx = geompy.MinDistance(vx, face)
+                distx += geompy.MinDistance(vx, nappes[k])
+                logging.debug("vertex distance: %s", distx)
+                if distx < 0.005:
+                  vxnouv.append(vx)
+              logging.debug("nombre vertex candidats %s", len(vxnouv))
+              if len(vxnouv) >= 2:
+                eds = [geompy.MakeEdge(vxnouv[j],vxnouv[(j+1)%len(vxnouv)]) for j in range(len(vxnouv))]
+                [edsorted2, minl,maxl] = sortEdges(eds)
+                edge = edsorted2[-1]
+                logging.debug("lg edge: %s", maxl)
+              else:
+                logging.debug("problème recherche edge radiale %s non résolu", k)
           edges.append(edge)
           name = 'edgeEndPipe%d'%k
           geomPublish(initLog.debug, edge, name)
       listEdges.append(edges)
-      
+
   return (listEdges, idFacesDebouchantes)
