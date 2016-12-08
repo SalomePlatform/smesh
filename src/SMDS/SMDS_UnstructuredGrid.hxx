eae69d8712b6ef30a734d44d9a8f6777e5c4becd
ana <ana@opencascade.com>
2016-12-08 16:45:32 +0300

    Windows compatibility

--- src/SMDS/SMDS_UnstructuredGrid.hxx
+++ src/SMDS/SMDS_UnstructuredGrid.hxx
@@ -45,12 +45,13 @@
 // allow very huge polyhedrons in tests
 #define NBMAXNODESINCELL 5000
 
-// Keep compatibility with paraview 5.0.1
-#ifndef VTK_HAS_MTIME_TYPE
-#define VTK_HAS_MTIME_TYPE
-typedef unsigned long int vtkMTimeType;
+// Keep compatibility with paraview 5.0.1 on Linux
+#ifndef WIN32
+  #ifndef VTK_HAS_MTIME_TYPE
+  #define VTK_HAS_MTIME_TYPE
+  typedef unsigned long int vtkMTimeType;
+  #endif
 #endif
-
 
 class SMDS_Downward;
 class SMDS_Mesh;
