288e3019978ba486d36a65ac39f9d758c6e9ade7
Cédric Aguerre <cedric.aguerre@edf.fr>
2017-06-19 10:55:59 +0200

    Share test definition

--- doc/salome/examples/CTestTestfileInstall.cmake
+++ doc/salome/examples/CTestTestfileInstall.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2016  CEA/DEN, EDF R&D, OPEN CASCADE
+# Copyright (C) 2015-2017  CEA/DEN, EDF R&D, OPEN CASCADE
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,134 +17,15 @@
 # See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 #
 
+INCLUDE(tests.set)
+
 SET(SALOME_TEST_DRIVER "$ENV{ABSOLUTE_APPLI_PATH}/bin/salome/appliskel/salome_test_driver.py")
 SET(COMPONENT_NAME SMESH)
 SET(TIMEOUT        300)
 
-SET(GOOD_TESTS
-  cartesian_algo
-  creating_meshes_ex02
-  creating_meshes_ex04
-  creating_meshes_ex06
-  creating_meshes_ex07
-  creating_meshes_ex08
-  defining_hypotheses_ex01
-  defining_hypotheses_ex02
-  defining_hypotheses_ex03
-  defining_hypotheses_ex04
-  defining_hypotheses_ex05
-  defining_hypotheses_ex07
-  defining_hypotheses_ex08
-  defining_hypotheses_ex10
-  defining_hypotheses_ex11
-  defining_hypotheses_ex12
-  defining_hypotheses_ex13
-  defining_hypotheses_ex14
-  defining_hypotheses_ex15
-  defining_hypotheses_ex16
-  defining_hypotheses_adaptive1d
-  filters_ex01
-  filters_ex03
-  filters_ex04
-  filters_ex05
-  filters_ex06
-  filters_ex07
-  filters_ex09
-  filters_ex10
-  filters_ex11
-  filters_ex12
-  filters_ex13
-  filters_ex14
-  filters_ex15
-  filters_ex16
-  filters_ex17
-  filters_ex18
-  filters_ex19
-  filters_ex20
-  filters_ex21
-  filters_ex22
-  filters_ex26
-  filters_ex27
-  filters_ex28
-  filters_ex29
-  filters_ex30
-  filters_ex31
-  filters_ex33
-  filters_ex34
-  filters_ex36
-  grouping_elements_ex01
-  grouping_elements_ex02
-  grouping_elements_ex03
-  grouping_elements_ex04
-  grouping_elements_ex05
-  grouping_elements_ex06
-  grouping_elements_ex07
-  grouping_elements_ex08
-  measurements_ex01
-  measurements_ex02
-  modifying_meshes_ex01
-  modifying_meshes_ex02
-  modifying_meshes_ex03
-  modifying_meshes_ex04
-  modifying_meshes_ex05
-  modifying_meshes_ex06
-  modifying_meshes_ex07
-  modifying_meshes_ex08
-  modifying_meshes_ex09
-  modifying_meshes_ex10
-  modifying_meshes_ex11
-  modifying_meshes_ex12
-  modifying_meshes_ex13
-  modifying_meshes_ex14
-  modifying_meshes_ex15
-  modifying_meshes_ex16
-  modifying_meshes_ex17
-  modifying_meshes_ex18
-  modifying_meshes_ex19
-  modifying_meshes_ex20
-  modifying_meshes_ex21
-  modifying_meshes_ex22
-  modifying_meshes_ex23
-  modifying_meshes_ex24
-  modifying_meshes_ex25
-  prism_3d_algo
-  quality_controls_ex01
-  quality_controls_ex02
-  quality_controls_ex03
-  quality_controls_ex04
-  quality_controls_ex05
-  quality_controls_ex07
-  quality_controls_ex08
-  quality_controls_ex09
-  quality_controls_ex10
-  quality_controls_ex11
-  quality_controls_ex12
-  quality_controls_ex13
-  quality_controls_ex14
-  quality_controls_ex15
-  quality_controls_ex16
-  quality_controls_ex17
-  quality_controls_ex18
-  quality_controls_ex19
-  transforming_meshes_ex01
-  transforming_meshes_ex02
-  transforming_meshes_ex03
-  transforming_meshes_ex04
-  transforming_meshes_ex05
-  transforming_meshes_ex06
-  transforming_meshes_ex07
-  transforming_meshes_ex08
-  transforming_meshes_ex09
-  transforming_meshes_ex10
-  transforming_meshes_ex11
-  transforming_meshes_ex12
-  transforming_meshes_ex13
-  use_existing_faces
-  viewing_meshes_ex02
-)
-
 FOREACH(tfile ${GOOD_TESTS})
-  SET(TEST_NAME SMESH_${tfile})
-  ADD_TEST(${TEST_NAME} python ${SALOME_TEST_DRIVER} ${TIMEOUT} ${tfile}.py)
+  GET_FILENAME_COMPONENT(BASE_NAME ${tfile} NAME_WE)
+  SET(TEST_NAME SMESH_${BASE_NAME})
+  ADD_TEST(${TEST_NAME} python ${SALOME_TEST_DRIVER} ${TIMEOUT} ${tfile})
   SET_TESTS_PROPERTIES(${TEST_NAME} PROPERTIES LABELS "${COMPONENT_NAME}")
 ENDFOREACH()
