SalomePlatform/smesh

commit 49ff23c94f469a0bd9de9981fb709eef8a8c4417

reintroduction of choice of MED minor version when exporting MED files

src/SMESH/SMESH_Mesh.cxx
@@ -1402,6 +1402,10 @@ bool SMESH_Mesh::HasDuplicatedGroupNamesMED()
  *  \param [in] theAutoGroups - boolean parameter for creating/not creating
  *              the groups Group_On_All_Nodes, Group_On_All_Faces, ... ;
  *              the typical use is auto_groups=false.
+ *  \param [in] theMinor - define the minor version (y, where version is x.y.z) of MED file format.
+ *              The theMinor must be between 0 and the current minor version of MED file library.
+ *              If theMinor is equal to -1, the minor version is not changed (default).
+ *              The major version (x, where version is x.y.z) cannot be changed.
  *  \param [in] meshPart - mesh data to export
  *  \param [in] theAutoDimension - if \c true, a space dimension of a MED mesh can be either
      *         - 1D if all mesh nodes lie on OX coordinate axis, or
@@ -1417,6 +1421,7 @@ bool SMESH_Mesh::HasDuplicatedGroupNamesMED()
 void SMESH_Mesh::ExportMED(const char *        file, 
                            const char*         theMeshName, 
                            bool                theAutoGroups,
+                           int                 theMinor,
                            const SMESHDS_Mesh* meshPart,
                            bool                theAutoDimension,
                            bool                theAddODOnVertices,
@@ -1427,7 +1432,7 @@ void SMESH_Mesh::ExportMED(const char *        file,
   SMESH_TRY;
 
   DriverMED_W_SMESHDS_Mesh myWriter;
-  myWriter.SetFile         ( file );
+  myWriter.SetFile         ( file , theMinor);
   myWriter.SetMesh         ( meshPart ? (SMESHDS_Mesh*) meshPart : _myMeshDS   );
   myWriter.SetAutoDimension( theAutoDimension );
   myWriter.AddODOnVertices ( theAddODOnVertices );

idl/SMESH_Mesh.idl
@@ -622,6 +622,10 @@ module SMESH
      *                 the groups Group_On_All_Nodes, Group_On_All_Faces, ... ;
      *                 the typical use is auto_groups=false.
      * - overwrite : boolean parameter for overwriting/not overwriting the file, if it exists
+     * - minor : define the minor version of MED file format.
+     *           The minor must be between 0 and the current minor version of MED file library.
+     *           If minor is equal to -1, the minor version is not changed (default).
+     *           The major version cannot be changed.
      * - autoDimension : if @c true, a space dimension of a MED mesh can be either
      *         - 1D if all mesh nodes lie on OX coordinate axis, or
      *         - 2D if all mesh nodes lie on XOY coordinate plane, or
@@ -630,6 +634,7 @@ module SMESH
      */
     void ExportMED( in string      fileName, 
 		    in boolean     auto_groups, 
+		    in long        minor,
 		    in boolean     overwrite,
 		    in boolean     autoDimension) raises (SALOME::SALOME_Exception);
 
@@ -639,6 +644,10 @@ module SMESH
      * - meshPart : a part of mesh to store
      * - fileName : name of the MED file
      * - overwrite : boolean parameter for overwriting/not overwriting the file, if it exists
+     * - minor : define the minor version (y, where version is x.y.z) of MED file format.
+     *           The minor must be between 0 and the current minor version of MED file library.
+     *           If minor is equal to -1, the minor version is not changed (default).
+     *           The major version (x, where version is x.y.z) cannot be changed.
      * - autoDimension : if @c True, a space dimension for export is defined by mesh 
      *                 configuration; for example a planar mesh lying on XOY plane
      *                 will be exported as a mesh in 2D space. 
@@ -654,6 +663,7 @@ module SMESH
     void ExportPartToMED( in SMESH_IDSource     meshPart,
                           in string             fileName,
                           in boolean            auto_groups,
+                          in long               minor,
                           in boolean            overwrite,
                           in boolean            autoDimension,
                           in GEOM::ListOfFields fields,
@@ -665,6 +675,11 @@ module SMESH
      */
     void ExportSAUV( in string file, in boolean auto_groups )
       raises (SALOME::SALOME_Exception);
+    
+    /*!
+     * Return string representation of a MED file version comprising nbDigits
+     */
+    string GetVersionString(in long minor, in short nbDigits);
 
     /*!
      * Export Mesh to different Formats

src/DriverMED/CMakeLists.txt
@@ -20,6 +20,7 @@
 # --- options ---
 # additional include directories
 INCLUDE_DIRECTORIES(
+  ${MEDFILE_INCLUDE_DIRS}
   ${HDF5_INCLUDE_DIRS}
   ${KERNEL_INCLUDE_DIRS}
   ${OpenCASCADE_INCLUDE_DIR}

src/DriverMED/DriverMED_W_SMESHDS_Mesh.cxx
@@ -35,6 +35,8 @@
 #include "SMDS_SetIterator.hxx"
 #include "SMESHDS_Mesh.hxx"
 
+#include <med.h>
+
 #include <BRep_Tool.hxx>
 #include <TopExp_Explorer.hxx>
 #include <TopoDS.hxx>
@@ -62,9 +64,39 @@ DriverMED_W_SMESHDS_Mesh::DriverMED_W_SMESHDS_Mesh():
   myDoAllInGroups(false)
 {}
 
-void DriverMED_W_SMESHDS_Mesh::SetFile(const std::string& theFileName)
+void DriverMED_W_SMESHDS_Mesh::SetFile(const std::string& theFileName, int theMinor)
 {
+  myMinor = theMinor;
   Driver_SMESHDS_Mesh::SetFile(theFileName);
+}
+
+/*!
+ * MED version is either the latest available, or with an inferior minor,
+ * to ensure backward compatibility on writing med files.
+ */
+string DriverMED_W_SMESHDS_Mesh::GetVersionString(int theVersion, int theNbDigits)
+{
+  TInt majeur, mineur, release;
+  majeur=MED_MAJOR_NUM;
+  mineur=MED_MINOR_NUM;
+  release=MED_RELEASE_NUM;
+  TInt imposedMineur = mineur;
+
+  if (theVersion < 0)
+    imposedMineur = mineur;
+  else if (theVersion > MED_MINOR_NUM)
+    imposedMineur = mineur;
+  else
+    imposedMineur = theVersion;
+
+  ostringstream name;
+  if ( theNbDigits > 0 )
+    name << majeur;
+  if ( theNbDigits > 1 )
+    name << "." << imposedMineur;
+  if ( theNbDigits > 2 )
+    name << "." << release;
+  return name.str();
 }
 
 void DriverMED_W_SMESHDS_Mesh::AddGroup(SMESHDS_GroupBase* theGroup)
@@ -425,7 +457,7 @@ Driver_Mesh::Status DriverMED_W_SMESHDS_Mesh::Perform()
       }
     }
 
-    MED::PWrapper myMed = CrWrapperW(myFile);
+    MED::PWrapper myMed = CrWrapperW(myFile, myMinor);
     PMeshInfo aMeshInfo = myMed->CrMeshInfo(aMeshDimension,aSpaceDimension,aMeshName);
     //MESSAGE("Add - aMeshName : "<<aMeshName<<"; "<<aMeshInfo->GetName());
     myMed->SetMeshInfo(aMeshInfo);

src/DriverMED/DriverMED_W_SMESHDS_Mesh.h
@@ -48,8 +48,10 @@ class MESHDRIVERMED_EXPORT DriverMED_W_SMESHDS_Mesh: public Driver_SMESHDS_Mesh
 
   DriverMED_W_SMESHDS_Mesh();
 
-  void SetFile(const std::string& theFileName);
+  void SetFile(const std::string& theFileName, int theMinor=-1);
   void SetAutoDimension(bool toFindOutDimension) { myAutoDimension = toFindOutDimension; }
+
+  static std::string GetVersionString(int theVersion, int theNbDigits=2);
 
   void AddGroupOfNodes();
   void AddGroupOfEdges();
@@ -87,6 +89,7 @@ class MESHDRIVERMED_EXPORT DriverMED_W_SMESHDS_Mesh: public Driver_SMESHDS_Mesh
   bool myAutoDimension;
   bool myAddODOnVertices;
   bool myDoAllInGroups;
+  int myMinor;
 };
 
 #endif

src/MEDWrapper/MED_Factory.cxx
@@ -111,10 +111,10 @@ namespace MED
     return new MED::TWrapper(fileName);
   }
 
-  PWrapper CrWrapperW(const std::string& fileName)
+  PWrapper CrWrapperW(const std::string& fileName, int theMinor)
   {
     if (!CheckCompatibility(fileName))
       remove(fileName.c_str());
-    return new MED::TWrapper(fileName);
+    return new MED::TWrapper(fileName, theMinor);
   }
 }

src/MEDWrapper/MED_Factory.hxx
@@ -43,7 +43,7 @@ namespace MED
   PWrapper CrWrapperR( const std::string& );
 
   MEDWRAPPER_EXPORT
-  PWrapper CrWrapperW( const std::string& );
+  PWrapper CrWrapperW( const std::string&, int theMinor=-1 );
 }
 
 #endif // MED_Factory_HeaderFile

src/MEDWrapper/MED_Wrapper.cxx
@@ -26,6 +26,7 @@
 
 #include <med.h>
 #include <med_err.h>
+#include <med_proto.h>
 
 #include <boost/version.hpp>
 
@@ -77,11 +78,14 @@ namespace MED
     TFile(const TFile&);
 
   public:
-    TFile(const std::string& theFileName):
+    TFile(const std::string& theFileName, TInt theMinor=-1):
       myCount(0),
       myFid(0),
-      myFileName(theFileName)
-    {}
+      myFileName(theFileName),
+      myMinor(theMinor)
+    {
+      if ((myMinor < 0) || (myMinor > MED_MINOR_NUM)) myMinor = MED_MINOR_NUM;
+    }
 
     ~TFile()
     {
@@ -94,12 +98,12 @@ namespace MED
     {
       if (myCount++ == 0) {
         const char* aFileName = myFileName.c_str();
-        myFid = MEDfileOpen(aFileName, med_access_mode(theMode));
+        myFid = MEDfileVersionOpen(aFileName,med_access_mode(theMode), MED_MAJOR_NUM, myMinor, MED_RELEASE_NUM);
       }
       if (theErr)
         *theErr = TErr(myFid);
       else if (myFid < 0)
-        EXCEPTION(std::runtime_error, "TFile - MEDfileOpen('"<<myFileName<<"',"<<theMode<<")");
+        EXCEPTION(std::runtime_error,"TFile - MEDfileVersionOpen('"<<myFileName<<"',"<<theMode<<"',"<< MED_MAJOR_NUM<<"',"<< myMinor<<"',"<< MED_RELEASE_NUM<<")");
     }
 
     const TIdt&
@@ -121,19 +125,24 @@ namespace MED
     TInt myCount;
     TIdt myFid;
     std::string myFileName;
+    TInt myMinor;
   };
 
   //---------------------------------------------------------------
   class TFileWrapper
   {
     PFile myFile;
+    TInt myMinor;
 
   public:
     TFileWrapper(const PFile& theFile,
                  EModeAcces theMode,
-                 TErr* theErr = NULL):
-      myFile(theFile)
+                 TErr* theErr = NULL,
+                 TInt theMinor=-1):
+      myFile(theFile),
+      myMinor(theMinor)
     {
+      if (myMinor < 0) myMinor = MED_MINOR_NUM;
       myFile->Open(theMode, theErr);
     }
 
@@ -178,8 +187,9 @@ namespace MED
 
   //---------------------------------------------------------------
   TWrapper
-  ::TWrapper(const std::string& theFileName):
-    myFile(new TFile(theFileName))
+  ::TWrapper(const std::string& theFileName, TInt theMinor):
+    myMinor(theMinor),
+    myFile(new TFile(theFileName, theMinor))
   {
     TErr aRet;
     myFile->Open(eLECTURE_ECRITURE, &aRet);
@@ -208,7 +218,7 @@ namespace MED
   TWrapper
   ::GetNbMeshes(TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return -1;
@@ -223,7 +233,7 @@ namespace MED
                 MED::TMeshInfo& theInfo,
                 TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -284,7 +294,7 @@ namespace MED
                 EModeAcces theMode,
                 TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -368,7 +378,7 @@ namespace MED
   ::GetNbFamilies(const MED::TMeshInfo& theInfo,
                   TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return -1;
@@ -385,7 +395,7 @@ namespace MED
                  const MED::TMeshInfo& theInfo,
                  TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return -1;
@@ -404,7 +414,7 @@ namespace MED
                   const MED::TMeshInfo& theInfo,
                   TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return -1;
@@ -423,7 +433,7 @@ namespace MED
                   MED::TFamilyInfo& theInfo,
                   TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -481,7 +491,7 @@ namespace MED
                   EModeAcces theMode,
                   TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -603,7 +613,7 @@ namespace MED
              EGeometrieElement theGeom,
              TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -652,7 +662,7 @@ namespace MED
              EGeometrieElement theGeom,
              TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -694,7 +704,7 @@ namespace MED
                   EGeometrieElement theGeom,
                   TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -743,7 +753,7 @@ namespace MED
                   EGeometrieElement theGeom,
                   TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -785,7 +795,7 @@ namespace MED
                 EGeometrieElement theGeom,
                 TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -843,7 +853,7 @@ namespace MED
                 EGeometrieElement theGeom,
                 TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -890,7 +900,7 @@ namespace MED
                ETable theTable,
                TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return -1;
@@ -918,7 +928,7 @@ namespace MED
   ::GetNodeInfo(MED::TNodeInfo& theInfo,
                 TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -1009,7 +1019,7 @@ namespace MED
                 EModeAcces theMode,
                 TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -1320,7 +1330,7 @@ namespace MED
                         EConnectivite theConnMode,
                         TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return 0;
@@ -1353,7 +1363,7 @@ namespace MED
   ::GetPolygoneInfo(MED::TPolygoneInfo& theInfo,
                     TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -1412,7 +1422,7 @@ namespace MED
                     EModeAcces theMode,
                     TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -1564,7 +1574,7 @@ namespace MED
                         EConnectivite theConnMode,
                         TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       EXCEPTION(std::runtime_error, "GetPolyedreConnSize - (...)");
@@ -1610,7 +1620,7 @@ namespace MED
   ::GetPolyedreInfo(TPolyedreInfo& theInfo,
                     TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -1673,7 +1683,7 @@ namespace MED
                     EModeAcces theMode,
                     TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -1869,7 +1879,7 @@ namespace MED
   {
     TEntityInfo anInfo;
 
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return anInfo;
@@ -1981,7 +1991,7 @@ namespace MED
                EConnectivite theConnMode,
                TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return -1;
@@ -2030,7 +2040,7 @@ namespace MED
   ::GetCellInfo(MED::TCellInfo& theInfo,
                 TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -2097,7 +2107,7 @@ namespace MED
                 EModeAcces theMode,
                 TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -2275,7 +2285,8 @@ namespace MED
   TWrapper
   ::GetBallGeom(const TMeshInfo& theMeshInfo)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE);
+    TErr anError;
+    TFileWrapper aFileWrapper(myFile, eLECTURE, &anError, myMinor);
 
     // read med_geometry_type of "MED_BALL" element
     char geotypename[ MED_NAME_SIZE + 1] = MED_BALL_NAME;
@@ -2287,7 +2298,8 @@ namespace MED
   TWrapper
   ::GetNbBalls(const TMeshInfo& theMeshInfo)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE);
+    TErr anError;
+    TFileWrapper aFileWrapper(myFile, eLECTURE, &anError, myMinor);
 
     EGeometrieElement ballType = GetBallGeom(theMeshInfo);
     if (ballType < 0)
@@ -2302,7 +2314,7 @@ namespace MED
   ::GetBallInfo(TBallInfo& theInfo,
                 TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     // check geometry of MED_BALL
     if (theInfo.myGeom == eBALL)
@@ -2352,7 +2364,7 @@ namespace MED
                 EModeAcces theMode,
                 TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     TErr ret;
     char ballsupportname[MED_NAME_SIZE+1] = "BALL_SUPPORT_MESH";
@@ -2480,7 +2492,7 @@ namespace MED
   TWrapper
   ::GetNbFields(TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return -1;
@@ -2494,7 +2506,7 @@ namespace MED
   ::GetNbComp(TInt theFieldId,
               TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return -1;
@@ -2509,7 +2521,7 @@ namespace MED
                  MED::TFieldInfo& theInfo,
                  TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -2574,7 +2586,7 @@ namespace MED
                  EModeAcces theMode,
                  TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -2661,7 +2673,7 @@ namespace MED
   TWrapper
   ::GetNbGauss(TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return -1;
@@ -2675,7 +2687,7 @@ namespace MED
   ::GetGaussPreInfo(TInt theId,
                     TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return TGaussInfo::TInfo(TGaussInfo::TKey(ePOINT1, ""), 0);
@@ -2715,7 +2727,7 @@ namespace MED
                  TGaussInfo& theInfo,
                  TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -2761,7 +2773,7 @@ namespace MED
                     TErr* theErr)
   {
     theEntity = EEntiteMaillage(-1);
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr) {
       if (theEntityInfo.empty())
@@ -2884,7 +2896,7 @@ namespace MED
                      MED::TTimeStampInfo& theInfo,
                      TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     const TGeom2Size& aGeom2Size = theInfo.myGeom2Size;
 
@@ -3037,7 +3049,7 @@ namespace MED
   TWrapper
   ::GetNbProfiles(TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return -1;
@@ -3051,7 +3063,7 @@ namespace MED
   ::GetProfilePreInfo(TInt theId,
                       TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return TProfileInfo::TInfo();
@@ -3079,7 +3091,7 @@ namespace MED
                    TProfileInfo& theInfo,
                    TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -3124,7 +3136,7 @@ namespace MED
                    EModeAcces theMode,
                    TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -3177,7 +3189,7 @@ namespace MED
                       const TKey2Gauss& theKey2Gauss,
                       TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -3361,7 +3373,7 @@ namespace MED
                       EModeAcces theMode,
                       TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -3698,7 +3710,7 @@ namespace MED
   ::GetGrilleInfo(TGrilleInfo& theInfo,
                   TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -3868,7 +3880,7 @@ namespace MED
   {
     if (theInfo.myMeshInfo->myType != eSTRUCTURE)
       return;
-    TFileWrapper aFileWrapper(myFile, theMode, theErr);
+    TFileWrapper aFileWrapper(myFile, theMode, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;
@@ -4003,7 +4015,7 @@ namespace MED
                   EGrilleType& theGridType,
                   TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       EXCEPTION(std::runtime_error, " GetGrilleType - aFileWrapper (...)");
@@ -4029,7 +4041,7 @@ namespace MED
                     TIntVector& theStruct,
                     TErr* theErr)
   {
-    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr);
+    TFileWrapper aFileWrapper(myFile, eLECTURE, theErr, myMinor);
 
     if (theErr && *theErr < 0)
       return;

commit b51815efc6922361b91af479621e99213a4f1497

Fix compilation pb caused by commit 49ff23c9

src/SMESH/SMESH_Mesh.cxx
@@ -1508,7 +1508,7 @@ void SMESH_Mesh::ExportSAUV(const char *file,
   cmd += "from medutilities import my_remove ; my_remove(r'" + medfilename + "')";
   cmd += "\"";
   system(cmd.c_str());
-  ExportMED(medfilename.c_str(), theMeshName, theAutoGroups,
+  ExportMED(medfilename.c_str(), theMeshName, theAutoGroups, /*minor=*/-1,
             /*meshPart=*/NULL, /*theAutoDimension=*/false, /*theAddODOnVertices=*/false,
             /*theAllElemsToGroup=*/true ); // theAllElemsToGroup is for PAL0023413
 #ifdef WIN32