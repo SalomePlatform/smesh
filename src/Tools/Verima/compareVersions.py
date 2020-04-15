0fc0831670e27a5611b941c52dc152fd63964515
vsr <vsr@opencascade.com>
2020-04-15 17:49:00 +0300

    Copyright update 2020

--- src/Tools/Verima/compareVersions.py
+++ src/Tools/Verima/compareVersions.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013-2019  CEA/DEN, EDF R&D
+# Copyright (C) 2013-2020  EDF R&D
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
