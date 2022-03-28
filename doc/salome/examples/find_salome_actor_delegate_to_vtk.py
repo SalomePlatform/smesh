a3351b3fb0c2104ca65a26e9ad04d9414fd69c52
vsr <vsr@opencascade.com>
2022-03-28 12:10:13 +0300

    bos #24180 This test is not needed anymore

--- doc/salome/examples/find_salome_actor_delegate_to_vtk.py
+++
@@ -1,9 +0,0 @@
-
-
-# Find if SALOME_ACTOR_DELEGATE_TO_VTK is activated
-
-import os
-import sys
-
-if 'SALOME_ACTOR_DELEGATE_TO_VTK' not in os.environ:
-    raise RuntimeError('SALOME_ACTOR_DELEGATE_TO_VTK is not set!')
