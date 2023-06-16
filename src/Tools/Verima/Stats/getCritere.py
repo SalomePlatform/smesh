7e714e8fc0d0884011007c9daed6e6e492402693
mbs <martin.bernhard@opencascade.com>
2023-06-16 20:28:19 +0100

    updated copyright message

--- src/Tools/Verima/Stats/getCritere.py
+++ src/Tools/Verima/Stats/getCritere.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2014-2022  EDF R&D
+# Copyright (C) 2014-2023  EDF R&D
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
